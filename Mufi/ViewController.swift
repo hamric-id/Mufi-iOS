5c022706c3f96c924d7d822fb784eae4af81e914
hamric-id <mhamzahr@gmail.com>
2026-08-15 00:24:59 +0700

    Initial Commit

---
+++ Mufi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mufi
+//
+//  Created by Muhammad Hamzah Robbani on 15/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MufiTests/MufiTests.swift
@@ -0,0 +1,19 @@
+//
+//  MufiTests.swift
+//  MufiTests
+//
+//  Created by Muhammad Hamzah Robbani on 15/08/26.
+//
+
+import Testing
+@testable import Mufi
+
+struct MufiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
